1dbce3387697cae3fe8438e29f5f001afacc2181
gpaezpy <gpaez.net@gmail.com>
2020-02-28 17:27:38 -0300

    varias actualizaciones

--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -1,55 +1,73 @@
 package com.bolsadeideas.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
-@Table(name="clientes")
-public class Cliente implements Serializable{
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
-	
-	@NotEmpty(message ="no puede estar vacio")
-	@Size(min=4, max=12, message="el tamaño tiene que estar entre 4 y 12")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty(message = "no puede estar vacio")
+	@Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12")
 	@Column(nullable = false)
 	private String nombre;
-	@NotEmpty(message ="no puede estar vacio")
-	@Size(min=4, max=12)
+
+	@NotEmpty(message = "no puede estar vacio")
 	private String apellido;
-	@NotEmpty(message ="no puede estar vacio")
-	@Email(message="no es una dirección de correo bien formada")
+
+	@NotEmpty(message = "no puede estar vacio")
+	@Email(message = "no es una dirección de correo bien formada")
 	@Column(nullable = false, unique = true)
 	private String email;
-	
-	@Column(name="create_at")
+
+	@NotNull(message = "no puede estar vacio")
+	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 
-	public long getId() {
+	private String foto;
+
+	@NotNull(message = "la región no puede ser vacia")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id")
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	private Region region;
+
+	@JsonIgnoreProperties(value={"cliente", "hibernateLazyInitializer", "handler"}, allowSetters=true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -85,9 +103,21 @@ public class Cliente implements Serializable{
 		this.createAt = createAt;
 	}
 
-	@PrePersist
-	public void prePersist(){
-		createAt = new Date();
+	public String getFoto() {
+		return foto;
 	}
 
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	private static final long serialVersionUID = 1L;
 }
--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -2,46 +2,56 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 @Service
-public class ClienteServiceImpl implements IClienteService{
-	
+public class ClienteServiceImpl implements IClienteService {
+
 	@Autowired
 	private IClienteDao clienteDao;
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
 		return (List<Cliente>) clienteDao.findAll();
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
 	public Cliente findById(Long id) {
-		// TODO Auto-generated method stub
 		return clienteDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public Cliente save(Cliente cliente) {
-		// TODO Auto-generated method stub
 		return clienteDao.save(cliente);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
 		clienteDao.deleteById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Region> findAllRegiones() {
+		return clienteDao.findAllRegiones();
 	}
 
 }
